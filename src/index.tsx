a6c1773ba4b475d6f1a1ff7e5eeebe286422305f
zYoru <xininfected@gmail.com>
2022-10-19 02:07:05 +0200

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Home } from "./Home";
+import "antd/dist/antd.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<Home />);
